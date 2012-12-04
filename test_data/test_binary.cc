45ee7b287ca13c02b7d7a0aba0808a82fd792fd3
Scott Graham <scottmg@chromium.org>
2012-12-03 22:17:28 -0800

    add test binary

---
+++ test_data/test_binary.cc
@@ -0,0 +1,16 @@
+// // Copyright 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Hopefully this will be first binary we can load successfully.
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  printf("This is the test binary!\n");
+  printf("argc: %d\n", argc);
+  for (int i = 0; i < argc; ++i) {
+    printf("argv[%d]: %s\n", i, argv[i]);
+  }
+  return 0;
+}
