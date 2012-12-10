9284ebd83599d6cd618a66fce977fdc0cf35707b
Scott Graham <scott.github@h4ck3r.net>
2012-12-09 23:41:34 -0800

    add functions to test binary

--- test_data/test_binary.cc
+++ test_data/test_binary.cc
@@ -7,6 +7,15 @@
 #include <stdio.h>
 #include <windows.h>
 
+int SubFunction(int input) {
+  return input * 10;
+}
+
+int Function() {
+  int x = SubFunction(44);
+  return x;
+}
+
 int main(int argc, char** argv) {
   printf("This is the test binary!\n");
   printf("argc: %d\n", argc);
@@ -15,6 +24,9 @@ int main(int argc, char** argv) {
     printf("argv[%d]: %s\n", i, argv[i]);
   }
   printf("f: %f\n", f);
+  int result = Function();
+  printf("result: %d\n", result);
+  printf("Function(): %d\n", Function());
   Sleep(10000);
   printf("after sleep\n");
   return 0;
